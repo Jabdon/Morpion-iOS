062cc037849711f52b3b8bc8500e3a01953a3f00
Jonathan Abdon <abdonjonathan@gmailcom>
2018-01-14 14:16:33 -0500

    improve Launch screen

--- Morpion/AppDelegate.swift
+++ Morpion/AppDelegate.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Fabric
+              Fabric.with([Crashlytics.self])
+        
         // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
